07a637e7563bb86efb0742689970a64380356e43
Yevgeniy Marchenko <Jackson.stethem@gmail.com>
2026-04-15 22:02:52 +0300

    Created app that will create every 1 second file

--- app/main.py
+++ app/main.py
@@ -1,9 +1,16 @@
-from datetime import datetime  # DO NOT CHANGE THIS IMPORT
+import time
+from datetime import datetime
 
 
-def main():
-    # write your code here
-    pass
+def main() -> None:
+    while True:
+        now = datetime.now()
+        file_name = f"app-{now.strftime('%H_%M_%S')}.log"
+        content = now.strftime("%Y-%m-%d %H:%M:%S")
+        print(f"{content} {file_name}")
+        with open(file_name, "w") as f:
+            f.write(content)
+        time.sleep(1)
 
 
 if __name__ == "__main__":
